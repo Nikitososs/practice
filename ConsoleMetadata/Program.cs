6d493626110c85d78e4b9e0d21d514e9dbfe3e33
Nikitososs <nsmirnow.n@gmail.com>
2025-07-05 23:13:06 +0600

    feat: Console app for displaying dll metadata

---
+++ ConsoleMetadata/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using LibInfoNs;
+
+namespace ConsoleMetadata;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string? path;
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Путь не был указан, введите путь:");
+            path = Console.ReadLine();
+        }
+        else path = args[0];
+        if (path != null)
+        {
+            Assembly lib = Assembly.LoadFrom(path);
+            LibInfo libInfo = new LibInfo(lib);
+            libInfo.Print();
+        }
+    }
+}
