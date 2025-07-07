9145686cace50d35ac188ba59db0c29ebad4eba7
Nikitososs <nsmirnow.n@gmail.com>
2025-07-08 04:04:43 +0600

    feat: add plugins for test

---
+++ Plugins/SecondPlugin/SecondPlugin.cs
@@ -0,0 +1,12 @@
+﻿using task10;
+
+namespace SecondPlugin;
+
+[PluginLoad([])]
+public class Plugin2 : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Выполнен второй плагин");
+    }
+}
