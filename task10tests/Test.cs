3ae3d96f7fb2a965e425b4e236dced6c8f887b19
Nikitososs <nsmirnow.n@gmail.com>
2025-07-08 04:05:28 +0600

    test: add tests for PluginLoader

---
+++ task10tests/Test.cs
@@ -0,0 +1,43 @@
+using task10;
+
+namespace task10tests;
+
+public class PluginLoaderTest
+{
+    [Fact]
+    public void PluginLoader_CorrectLoading_and_Executing()
+    {
+        var loader = new PluginLoader(Path.Combine("..", "..", "..", "PluginsForTest"));
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        var expectedValue = "Выполнен первый плагин\nВыполнен второй плагин\nВыполнен четвёртый плагин\nВыполнен третий плагин\n";
+
+        loader.Load();
+
+        Assert.Equal(expectedValue, output.ToString());
+    }
+
+    [Fact]
+    public void PluginLoader_PluginsNotFound_Ex()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+
+        var loader = new PluginLoader(testDir);
+
+        Assert.Throws<Exception>(loader.Load).Message.Equals("Плагины не найдены");
+
+        Directory.Delete(testDir, true);
+    }
+    
+    [Fact]
+    public void PluginLoader_DirectoryNotExists_Ex()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+
+        var loader = new PluginLoader(testDir);
+
+        Assert.Throws<Exception>(loader.Load).Message.Equals("Неверный путь");
+    }
+}
