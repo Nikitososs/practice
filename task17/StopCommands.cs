31acca0db429add355ab8a3ddb79f39e9583091c
Nikitososs <nsmirnow.n@gmail.com>
2025-07-16 04:19:57 +0600

    feat: add HardStop and SoftStop commands

---
+++ task17/StopCommands.cs
@@ -0,0 +1,21 @@
+namespace task17;
+
+public class HardStop(ServerThread server) : ICommand
+{
+    private readonly ServerThread _server = server;
+
+    public void Execute()
+    {
+        _server.HardStop();
+    }
+}
+
+public class SoftStop(ServerThread server) : ICommand
+{
+    private readonly ServerThread _server = server;
+
+    public void Execute()
+    {
+        _server.SoftStop();
+    }
+}
