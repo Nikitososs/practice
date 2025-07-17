2438719de8002cb41d596bfa353f9e9ae24215a5
Nikitososs <nsmirnow.n@gmail.com>
2025-07-17 19:14:58 +0600

    feat: add Console app for demonstating executions

---
+++ task19/Program.cs
@@ -0,0 +1,18 @@
+﻿using task18;
+
+namespace task19;
+
+public class Program
+{
+    public static void Main()
+    {
+        var server = new ServerThread();
+
+        Enumerable.Range(0, 5)
+            .ToList()
+            .ForEach(id => server.AddCommand(new TestCommand(id, 3)));
+
+        Thread.Sleep(100);
+        server.AddCommand(new HardStop(server));
+    }
+}
