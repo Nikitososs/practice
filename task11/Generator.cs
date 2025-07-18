d09d89e7b1d6c6005ed690e6ccae7d3707f750db
Nikitososs <nsmirnow.n@gmail.com>
2025-07-18 13:17:07 +0600

    feat: add class generator

---
+++ task11/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace task11;
+
+public class Generator
+{
+    public static dynamic GenerateClass(string code, string className)
+    {
+        using MemoryStream output = new();
+
+        CSharpCompilation.Create(
+            "Generated",
+            [CSharpSyntaxTree.ParseText(code)],
+            [MetadataReference.CreateFromFile(typeof(object).Assembly.Location)],
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+            .Emit(output);
+
+        return Activator.CreateInstance(Assembly.Load(output.ToArray()).GetType(className)!)!;
+    }
+}
